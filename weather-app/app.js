a96d437369efa923323b7031142b5bd5ba1e6942
danyalsiraj <danyal61@gmail.com>
2017-11-15 23:47:52 -0500

    fetch the long and lat from an address entered by user

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,8 +1,26 @@
 const request = require('request');
+const yargs = require('yargs');
 
+const argv = yargs
+  .options({ //ths make sures that we always enter the address
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'Address to get the weather',
+      string: true //this is to make sure that the address is taken in as a string
+    }
+
+  })
+  .help()
+  .alias('help', 'h') //sets an alia for help
+  .argv
+console.log(argv);
+var encodedAdress = encodeURIComponent(argv.address); //encodes the address string(the spaces in the string)
+//to decode use decodeURIComponent
+console.log(encodedAdress);
 //gets long and lat from google api
 request({
-  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=3422%20fountain%20park%20ave',
+  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAdress}`,
   json: true
 }, (error, response, body) => { //these are default arguments for request read documentation for more details
   //  console.log(body);
