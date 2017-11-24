c2ad7f627c6f458b8f4d368de87e2c81cbbbe51d
danyalsiraj <danyal61@gmail.com>
2017-11-23 23:34:25 -0500

    handlebars basic and rendering static pages using handlebars

--- web-server/server.js
+++ web-server/server.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const hbs = require('hbs'); //wecan pass in data into an html file using this, this allows us to create templates instead of rendering every page seperately
 
 var app = express();
-
+app.set('view engine', 'hbs'); //this is a key value pair it tells express what to use
 app.use(express.static(__dirname + '/public')); //to use the static directory. this uses the htmll page in public directory
 //static directory can be used to add js, html, css...
 
@@ -10,16 +11,25 @@ app.use(express.static(__dirname + '/public')); //to use the static directory. t
 app.get('/', (request, response) => {
   //  response.send('Hello Express!!');
   //  response.send('<h1>Hello Express!!</h1>');//html data
-  response.send({
-    name: 'Danyal',
-    age: '23'
-  });
+  // response.send({
+  //   name: 'Danyal',
+  //   age: '23'
+  // });
+  response.render('home.hbs', {
+    pageTitle: 'Home Page',
+    currentYear: new Date().getFullYear(),
+    welcomeMessage: 'welcome to my page'
+  })
 });
 
 //we can make multiple pages
 //this page loads whndn you visit the about url in this case localhost:3000/about
 app.get('/about', (request, response) => {
-  response.send('About Page');
+  //response.send('About Page');
+  response.render('about.hbs', { //we pass in the information to display
+    pageTitle: 'About page',
+    currentYear: new Date().getFullYear() //gets the current year
+  });
 })
 
 app.get('/bad', (request, response) => {
