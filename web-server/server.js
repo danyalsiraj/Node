a2015d99050c64abe8cbe02ec590562775689577
danyalsiraj <danyal61@gmail.com>
2017-11-25 15:03:53 -0500

    Express Middleware

--- web-server/server.js
+++ web-server/server.js
@@ -1,12 +1,34 @@
 const express = require('express');
 const hbs = require('hbs'); //wecan pass in data into an html file using this, this allows us to create templates instead of rendering every page seperately
+const fs = require('fs');
 
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials'); // this allows us to refractor the areas that are same on every page i.e. footers
 app.set('view engine', 'hbs'); //this is a key value pair it tells express what to use
-app.use(express.static(__dirname + '/public')); //to use the static directory. this uses the htmll page in public directory
+//app.use(express.static(__dirname + '/public')); //to use the static directory. this uses the htmll page in public directory
 //static directory can be used to add js, html, css...
+
+//middleware same get has respose and request but also has next. plogram does not move on until next is called
+app.use((request, response, next) => { //we can use it to create logs
+  var now = new Date().toString();
+  var log = `${now} : ${request.method} ${request.url}`; //this tells us waht page user requested
+  console.log(log); //print log
+  fs.appendFile('server.log', log + '\n', (err) => { //save the log in a file
+    if (err) {
+      console.log('unable to write to server log');
+    }
+  })
+
+  next(); //if we dont call next the app will not move on
+});
+// below is an example of how middleware can be used
+//app.use((request, response, next) => {
+//   response.render('maintenance.hbs', { //we pass in the information to display
+//     pageTitle: 'We will be right back!!'
+//   });
+// });
+app.use(express.static(__dirname + '/public')); //we moved it rom above because it still loads the help.html if it is before even when we say the page is under maintanence
 
 hbs.registerHelper('getCurrentYear', () => {
   return new Date().getFullYear()
